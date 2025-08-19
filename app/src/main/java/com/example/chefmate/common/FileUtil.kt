21b261f91ada2dcfe34ce00fff09974d2dccaee1
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-08-19 16:12:37 +0700

    add FileUtil to save image

---
+++ app/src/main/java/com/example/chefmate/common/FileUtil.kt
@@ -0,0 +1,34 @@
+package com.example.chefmate.common
+
+// Có thể đặt ở cuối tệp AddRecipeActivity.kt hoặc tạo một tệp FileUtil.kt mới
+import android.content.Context
+import android.net.Uri
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+fun saveImageToInternalStorage(context: Context, uri: Uri): String? {
+    val contentResolver = context.contentResolver
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+    val fileName = "IMG_${timeStamp}.jpg"
+    val appDir = File(context.filesDir, "recipe_images") // Thư mục con để lưu ảnh công thức
+    if (!appDir.exists()) {
+        appDir.mkdirs() // Tạo thư mục nếu nó chưa tồn tại
+    }
+    val imageFile = File(appDir, fileName)
+
+    try {
+        contentResolver.openInputStream(uri)?.use { inputStream ->
+            FileOutputStream(imageFile).use { outputStream ->
+                inputStream.copyTo(outputStream)
+            }
+        }
+        return imageFile.absolutePath // Trả về đường dẫn tuyệt đối của ảnh đã lưu
+    } catch (e: IOException) {
+        e.printStackTrace()
+        return null
+    }
+}
