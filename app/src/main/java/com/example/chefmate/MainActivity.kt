6fc5dc234e5ce06dc284a6cd4916e1b571eb1a04
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-08-19 15:52:49 +0700

    update MainActivity

--- app/src/main/java/com/example/chefmate/MainActivity.kt
+++ app/src/main/java/com/example/chefmate/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.chefmate
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.createGraph
+import com.example.chefmate.ui.main.MainAct
+import com.example.chefmate.ui.recipe.AddRecipeScreen
 import com.example.chefmate.ui.theme.ChefMateTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChefMateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen(this)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(activity: Activity) {
+    val context = LocalContext.current
+    val navController = rememberNavController()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        NavHost(navController = navController, graph = NavGraph(activity, navController))
+    }
 }
+
+fun NavGraph(activity: Activity, navController: NavController): NavGraph {
+    return navController.createGraph(startDestination = "mainAct") {
+        composable("mainAct") {
+            MainAct(navController)
+        }
+        composable("addRecipe") {
+            AddRecipeScreen(navController)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ChefMateTheme {
-        Greeting("Android")
-    }
+
 }
