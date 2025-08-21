efd080d2047cdf3d279015031e9e24e9cac40479
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-08-21 18:31:05 +0700

    update RecipeListActivity.kt

--- app/src/main/java/com/example/chefmate/ui/recipe/RecipeListActivity.kt
+++ app/src/main/java/com/example/chefmate/ui/recipe/RecipeListActivity.kt
@@ -5,45 +5,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.chefmate.common.BigCard
 import com.example.chefmate.common.Header
-import com.example.chefmate.database.AppDatabase
-import com.example.chefmate.database.entity.RecipeEntity
 import com.example.chefmate.database.entity.toRecipe
-import com.example.chefmate.model.Recipe
-import com.example.chefmate.repository.RecipeRepository
 import com.example.chefmate.viewmodel.RecipeViewModel
-import com.example.chefmate.viewmodel.RecipeViewModelFactory
 
 @Composable
-fun RecipeListScreen() {
-    val context = LocalContext.current
-    val db = remember { AppDatabase.getDatabase(context) }
-    val recipeDao = remember { db.recipeDao() }
-    val ingredientDao = remember { db.ingredientDao() }
-    val stepDao = remember { db.stepDao() }
-    // Khởi tạo Repository
-    val recipeRepository = remember { RecipeRepository(recipeDao, ingredientDao, stepDao) }
-    // Khởi tạo ViewModel với Repository
-    val recipeViewModel: RecipeViewModel = viewModel(
-        factory = RecipeViewModelFactory(recipeRepository)
-    )
+fun RecipeListScreen(navController: NavController, recipeViewModel: RecipeViewModel = hiltViewModel()) {
     val listRecipes by recipeViewModel.allRecipes.collectAsState(initial = emptyList())
     Column(
         modifier = Modifier
@@ -56,50 +38,6 @@ fun RecipeListScreen() {
             modifier = Modifier
                 .height(50.dp)
         )
-//        val recipe1 = Recipe(
-//            1,
-//            1,
-//            "Sườn xào",
-//            "https://cdn.tgdd.vn/2021/10/CookDish/cach-lam-nuoc-mam-tac-avt-1200x676.jpg",
-//            "2",
-//            3,
-//            3,
-//            4,
-//            "",
-//            "Thanh",
-//            "",
-//            false
-//        )
-//
-//        val recipe2 = Recipe(
-//            1,
-//            1,
-//            "Sườn xào",
-//            "https://cdn.tgdd.vn/2021/10/CookDish/cach-lam-nuoc-mam-tac-avt-1200x676.jpg",
-//            "2",
-//            3,
-//            3,
-//            4,
-//            "",
-//            "Thanh",
-//            "",
-//            false
-//        )
-//        val recipe3 = Recipe(
-//            1,
-//            1,
-//            "Sườn xào",
-//            "https://cdn.tgdd.vn/2021/10/CookDish/cach-lam-nuoc-mam-tac-avt-1200x676.jpg",
-//            "2",
-//            3,
-//            3,
-//            4,
-//            "",
-//            "Thanh",
-//            "",
-//            false
-//        )
-//        val listRecipes = listOf(recipe1, recipe2, recipe3)
         LazyColumn(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
@@ -107,7 +45,9 @@ fun RecipeListScreen() {
                 .background(Color(0xFFFFFFFF))
         ) {
             items(listRecipes.size) { index ->
-                BigCard(listRecipes[index].toRecipe())
+                BigCard(listRecipes[index].toRecipe()) {
+                    navController.navigate("recipeDetail/${listRecipes[index].recipeId}")
+                }
             }
         }
     }
@@ -116,5 +56,5 @@ fun RecipeListScreen() {
 @Preview
 @Composable
 fun RecipeListScreenPreview() {
-    RecipeListScreen()
+    RecipeListScreen(rememberNavController())
 }
