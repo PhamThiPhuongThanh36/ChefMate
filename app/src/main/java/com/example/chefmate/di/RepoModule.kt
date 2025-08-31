22af564e3d6d89304cff692b940a0c1f565f90e8
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-08-31 23:40:39 +0700

    add repoModule

---
+++ app/src/main/java/com/example/chefmate/di/RepoModule.kt
@@ -0,0 +1,36 @@
+package com.example.chefmate.di
+
+import com.example.chefmate.database.dao.IngredientDao
+import com.example.chefmate.database.dao.RecipeDao
+import com.example.chefmate.database.dao.ShoppingDao
+import com.example.chefmate.database.dao.StepDao
+import com.example.chefmate.repository.RecipeRepository
+import com.example.chefmate.repository.ShoppingRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepoModule {
+    @Singleton
+    @Provides
+    fun provideRecipeRepository(
+        recipeDao: RecipeDao,
+        ingredientDao: IngredientDao,
+        stepDao: StepDao
+    ): RecipeRepository {
+        return RecipeRepository(recipeDao, ingredientDao, stepDao)
+    }
+
+    @Singleton
+    @Provides
+    fun provideShoppingRepository(
+        shoppingDao: ShoppingDao,
+        ingredientDao: IngredientDao,
+    ): ShoppingRepository {
+        return ShoppingRepository(shoppingDao, ingredientDao)
+    }
+}
