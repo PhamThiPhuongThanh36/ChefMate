d416bb121583b92867e9b6a9964b1626bbad7737
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-08-31 23:42:47 +0700

    update database ingredient

--- app/src/main/java/com/example/chefmate/database/dao/IngredientDao.kt
+++ app/src/main/java/com/example/chefmate/database/dao/IngredientDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.chefmate.database.entity.IngredientEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -13,6 +14,14 @@ interface IngredientDao {
     fun getIngredientsByRecipeId(recipeId: Int): Flow<List<IngredientEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertIngredients(ingredients: List<IngredientEntity>)
+    suspend fun insertIngredients(ingredients: List<IngredientEntity>)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertIngredient(ingredient: IngredientEntity): Long
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateIngredient(ingredient: IngredientEntity)
+
+    @Query("Select * from Ingredients where ingredientId = :ingredientId")
+    fun getIngredientById(ingredientId: Int): Flow<IngredientEntity>
 }
