477e27998eae773d82d511dba70a393cdc7b07c0
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-08-31 23:42:14 +0700

    add DataStoreHelper.kt

---
+++ app/src/main/java/com/example/chefmate/helper/DataStoreHelper.kt
@@ -0,0 +1,38 @@
+package com.example.chefmate.helper
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "dataStore")
+val LAST_SHOPPING_ID = intPreferencesKey("last_shopping_id")
+val IS_FINISHED_SHOPPING = booleanPreferencesKey("is_finished_shopping")
+object DataStoreHelper {
+    suspend fun updateLastShopping(context: Context, lastShoppingId: Int) {
+        context.dataStore.edit { preferences ->
+            preferences[LAST_SHOPPING_ID] = lastShoppingId
+            preferences[IS_FINISHED_SHOPPING] = false
+        }
+    }
+
+    suspend fun isFinishedShopping(context: Context): Boolean {
+        val preferences = context.dataStore.data.first()
+        return preferences[IS_FINISHED_SHOPPING] ?: true
+    }
+
+    suspend fun getLastShoppingId(context: Context): Int {
+        val preferences = context.dataStore.data.first()
+        return preferences[LAST_SHOPPING_ID] ?: 0
+    }
+
+    suspend fun finishShopping(context: Context) {
+        context.dataStore.edit { preferences ->
+            preferences[IS_FINISHED_SHOPPING] = true
+        }
+    }
+}
