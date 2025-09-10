019c5dfba40ce29573d8bddc4bae2c69d467852e
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-09-10 23:48:35 +0700

    add CommonHelper

---
+++ app/src/main/java/com/example/chefmate/helper/CommonHelper.kt
@@ -0,0 +1,99 @@
+package com.example.chefmate.helper
+
+import android.annotation.SuppressLint
+import com.example.chefmate.database.entity.RecipeEntity
+import com.example.chefmate.database.entity.ShoppingItemEntity
+import java.text.Normalizer
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+object CommonHelper {
+    @SuppressLint("DefaultLocale")
+    fun parseNumber(number: Int): String {
+        return when {
+            number < 1000 -> "$number"
+            number < 1000000 -> String.format("%.1fK", number / 1000f)
+            else -> String.format("%.1fM", number / 1000000f)
+        }
+    }
+
+    fun parseTime(time: String): String {
+        val isoFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+        isoFormat.timeZone = TimeZone.getTimeZone("Asia/Bangkok")
+        val dateTime = isoFormat.parse(time) ?: return ""
+
+        val now = Date()
+
+        val diffMillis = now.time - dateTime.time
+
+        val diffSeconds = diffMillis / 1000
+        val diffMinutes = diffSeconds / 60
+        val diffHours = diffMinutes / 60
+        val diffDays = diffHours / 24
+        val diffWeeks = diffDays / 7
+        val diffMonths = diffDays / 30
+        val diffYears = diffDays / 365
+
+        return when {
+            diffYears > 0 -> "$diffYears năm trước"
+            diffMonths > 0 -> "$diffMonths tháng trước"
+            diffWeeks > 0 -> "$diffWeeks tuần trước"
+            diffDays > 0 -> "$diffDays ngày trước"
+            diffHours > 0 -> "$diffHours giờ trước"
+            diffMinutes > 0 -> "$diffMinutes phút trước"
+            else -> "$diffSeconds giây trước"
+        }
+    }
+
+    fun parseTimeToDMY(time: String): String {
+        val isoFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+        isoFormat.timeZone = TimeZone.getTimeZone("UTC")
+
+        return try {
+            val date = isoFormat.parse(time)
+            val outputFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            outputFormat.format(date ?: return time)
+        } catch (e: Exception) {
+            time
+        }
+    }
+
+    fun toIso8601UTC(date: Date): String {
+        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+        sdf.timeZone = TimeZone.getTimeZone("UTC")
+        return sdf.format(date)
+    }
+
+    fun parseName(name: String): String {
+        val normalized = Normalizer.normalize(name, Normalizer.Form.NFD)
+        val noAccents = normalized.replace("\\p{InCombiningDiacriticalMarks}+".toRegex(), "")
+            .replace('đ', 'd')
+            .replace('Đ', 'D')
+
+        return noAccents
+            .trim()
+            .split("\\s+".toRegex())
+            .joinToString("_")
+    }
+
+    fun consolidateIngredients(ingredients: List<ShoppingItemEntity>? = null): List<ShoppingItemEntity> {
+        val consolidated = mutableListOf<ShoppingItemEntity>()
+        ingredients?.let { listIngredient ->
+            listIngredient.forEach { ingredient ->
+                val exitIngredient = consolidated.find {
+                    it.siName == ingredient.siName && it.siUnit == ingredient.siUnit
+                }
+                if (exitIngredient != null) {
+                    var newWeight = exitIngredient.siWeight.toInt() + ingredient.siWeight.toInt()
+                    consolidated.remove(exitIngredient)
+                    consolidated.add(exitIngredient.copy(siWeight = newWeight.toString()))
+                } else {
+                    consolidated.add(ingredient)
+                }
+            }
+        }
+        return consolidated
+    }
+}
