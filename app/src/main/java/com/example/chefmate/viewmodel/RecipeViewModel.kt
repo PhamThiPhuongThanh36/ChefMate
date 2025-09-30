33a99bed36f045d307e648db19866c76816dcedd
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-09-30 12:52:24 +0700

    update RecipeViewModel

--- app/src/main/java/com/example/chefmate/viewmodel/RecipeViewModel.kt
+++ app/src/main/java/com/example/chefmate/viewmodel/RecipeViewModel.kt
@@ -1,9 +1,7 @@
 package com.example.chefmate.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.example.chefmate.api.ApiClient
 import com.example.chefmate.database.entity.IngredientEntity
 import com.example.chefmate.database.entity.RecipeEntity
 import com.example.chefmate.database.entity.StepEntity
@@ -13,16 +11,13 @@ import com.example.chefmate.model.Recipe
 import com.example.chefmate.repository.RecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipeViewModel @Inject constructor(private val repository: RecipeRepository): ViewModel() {
-    private val _topTrending = MutableStateFlow<List<Recipe>>(emptyList())
-    val topTrending: StateFlow<List<Recipe>> = _topTrending
+
     val allRecipes: Flow<List<RecipeEntity>> = repository.getAllRecipes()
     suspend fun insertRecipe(recipe: RecipeEntity): Long {
         return repository.insertRecipe(recipe)
@@ -48,13 +43,13 @@ class RecipeViewModel @Inject constructor(private val repository: RecipeReposito
         return repository.getAllStepsByRecipeId(recipeId)
     }
 
-    suspend fun deleteRecipeById(recipeId: Int) {
+    fun deleteRecipeById(recipeId: Int) {
         viewModelScope.launch {
             repository.deleteRecipeById(recipeId)
         }
     }
 
-    suspend fun updateRecipe(recipe: RecipeEntity) {
+    fun updateRecipe(recipe: RecipeEntity) {
         viewModelScope.launch {
             repository.updateRecipe(recipe)
         }
@@ -68,13 +63,20 @@ class RecipeViewModel @Inject constructor(private val repository: RecipeReposito
         return repository.getTagsByRecipeId(recipeId)
     }
 
-    suspend fun getTopTrending(userId: Int? = null) {
-        val response = ApiClient.getTopTrending(userId)
-        response?.let {
-            response.data?.let {
-                _topTrending.value = it
-            }
-        }
+    suspend fun deleteTagsByRecipeId(recipeId: Int) {
+        repository.deleteTagsByRecipeId(recipeId)
+    }
+
+    suspend fun deleteIngredientsByRecipeId(recipeId: Int) {
+        repository.deleteIngredientsByRecipeId(recipeId)
+    }
+
+    suspend fun deleteStepsByRecipeId(recipeId: Int) {
+        repository.deleteStepsByRecipeId(recipeId)
+    }
+
+    fun searchRecipes(query: String): Flow<List<RecipeEntity>> {
+        return repository.searchRecipes(query)
     }
 }
 
