a9822941a2b1e1ce908d4c2e65ece83e920a0b25
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-11-09 14:45:51 +0700

    feat: add LoadingScreen

--- app/src/main/java/com/example/chefmate/common/Loadings.kt
+++ app/src/main/java/com/example/chefmate/common/Loadings.kt
@@ -1,6 +1,9 @@
 package com.example.chefmate.common
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
@@ -32,5 +35,22 @@ fun CircularLoading() {
                 )
             }
         }
+    }
+}
+
+@Composable
+fun LoadingScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFFFFF))
+    ) {
+        CircularProgressIndicator(
+            color = Color(0xFF757575),
+            modifier = Modifier
+                .size(50.dp)
+        )
     }
 }
