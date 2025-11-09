6426d3579cd8826943700f2273188172cff02fc6
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-11-09 14:46:54 +0700

    feat: get TopTrending from server

--- app/src/main/java/com/example/chefmate/ui/main/HomeScreen.kt
+++ app/src/main/java/com/example/chefmate/ui/main/HomeScreen.kt
@@ -16,7 +16,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,14 +28,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.chefmate.R
 import com.example.chefmate.model.Recipe
 import com.example.chefmate.common.*
 import com.example.chefmate.viewmodel.RecipeViewModel
-import kotlinx.coroutines.flow.map
 
 @Composable
-fun HomeScreen(recipeViewModel: RecipeViewModel) {
+fun HomeScreen(recipeViewModel: RecipeViewModel, navController: NavController) {
     val lazyListState = rememberLazyListState()
     val isShowPopular by remember { derivedStateOf { lazyListState.firstVisibleItemScrollOffset == 0 } }
     Log.d("HomeScreen", "isShowPopular: $isShowPopular")
@@ -46,23 +47,14 @@ fun HomeScreen(recipeViewModel: RecipeViewModel) {
             .fillMaxSize()
             .background(Color(0xFFFFFFFF))
     ) {
-        val recipes = recipeViewModel.allRecipes.map {
-            it.map { recipeEntity ->
-                Recipe(
-                    recipeId = recipeEntity.recipeId,
-                    recipeName = recipeEntity.recipeName,
-                    image = recipeEntity.image,
-                    cookingTime = recipeEntity.cookingTime,
-                    ration = recipeEntity.ration,
-                    viewCount = recipeEntity.viewCount,
-                    likeQuantity = recipeEntity.likeQuantity,
-                    isPublic = false,
-                    createdAt = "",
-                    userName = "",
-                    userImage = ""
-                )
+        var recipes by remember { mutableStateOf<List<Recipe>>(emptyList()) }
+        LaunchedEffect(Unit) {
+            recipes = recipeViewModel.getTopTrending()
+            recipes.forEachIndexed { i, r ->
+                Log.d("CheckRecipe", "[$i] name=${r.recipeName}, img=${r.image}, cook=${r.cookingTime}")
             }
-        }.collectAsState(initial = emptyList())
+        }
+        Log.d("TopTrending", "recipess: $recipes")
         var searchText by remember { mutableStateOf("") }
         val tagsList = listOf("đồ uống", "salad", "nước chấm", "món chính", "món súp", "món chay")
         val imgsList = listOf(R.drawable.lau, R.drawable.lauhaisan, R.drawable.matcha, R.drawable.matchayoguruto, R.drawable.misoshiru, R.drawable.misopoteto)
@@ -114,12 +106,12 @@ fun HomeScreen(recipeViewModel: RecipeViewModel) {
                 .fillMaxWidth()
                 .background(Color(0xFFFFFFFF))
         ) {
-            items(recipes.value.size) { index ->
+            items(recipes.size) { index ->
                 BigCard(
-                    recipe = recipes.value[index],
-                    onClick = {},
+                    recipe = recipes[index],
+                    onClick = { navController.navigate("recipeDetail/${recipes[index].recipeId}") },
                     modifier = Modifier
-                        .fillMaxWidth(0.9f),
+                        .fillMaxWidth(0.9f)
                 )
             }
         }
@@ -129,5 +121,5 @@ fun HomeScreen(recipeViewModel: RecipeViewModel) {
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen(hiltViewModel())
+    HomeScreen(hiltViewModel(), rememberNavController())
 }
