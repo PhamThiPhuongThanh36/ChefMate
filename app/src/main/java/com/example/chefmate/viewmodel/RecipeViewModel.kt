25f5a5bc36b231109c24c4e4f0ff93572d3795c5
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-11-09 14:54:28 +0700

    feat: get TopTrending from server

--- app/src/main/java/com/example/chefmate/viewmodel/RecipeViewModel.kt
+++ app/src/main/java/com/example/chefmate/viewmodel/RecipeViewModel.kt
@@ -2,35 +2,33 @@ package com.example.chefmate.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.chefmate.api.ApiClient
+import com.example.chefmate.api.ApiConstant
 import com.example.chefmate.database.entity.IngredientEntity
 import com.example.chefmate.database.entity.RecipeEntity
 import com.example.chefmate.database.entity.StepEntity
 import com.example.chefmate.database.entity.TagEntity
-import com.example.chefmate.database.entity.toRecipe
 import com.example.chefmate.model.Recipe
+import com.example.chefmate.model.RecipeView
 import com.example.chefmate.repository.RecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RecipeViewModel @Inject constructor(private val repository: RecipeRepository): ViewModel() {
+class RecipeViewModel @Inject constructor(private val repository: RecipeRepository) : ViewModel() {
+    val api = ApiClient.createService(ApiConstant.MAIN_URL)
 
     val allRecipes: Flow<List<RecipeEntity>> = repository.getAllRecipes()
     suspend fun insertRecipe(recipe: RecipeEntity): Long {
         return repository.insertRecipe(recipe)
     }
 
-    fun getRecipeById(recipeId: Int): Flow<Recipe?> {
-        return repository.getRecipeById(recipeId)
-            .map { entity -> entity?.toRecipe() }
-    }
-
     suspend fun insertIngredients(ingredients: List<IngredientEntity>) {
         repository.insertIngredients(ingredients)
     }
+
     suspend fun insertSteps(steps: List<StepEntity>) {
         repository.insertSteps(steps)
     }
@@ -78,5 +76,25 @@ class RecipeViewModel @Inject constructor(private val repository: RecipeReposito
     fun searchRecipes(query: String): Flow<List<RecipeEntity>> {
         return repository.searchRecipes(query)
     }
-}
 
+    suspend fun getTopTrending(): List<Recipe> {
+        return api.getTopTrending().map { r ->
+            r.copy(
+                isPublic = r.isPublic ?: true,
+                userName = r.userName ?: "",
+                userImage = r.userImage ?: "",
+                isLiked = r.isLiked ?: false
+            )
+        }
+    }
+
+    suspend fun getRecipeById(recipeId: Int): RecipeView? {
+        val localRecipe = repository.getRecipeByIdLocal(recipeId)
+        if (localRecipe != null) {
+            return localRecipe
+        }
+        val remoteRecipe = repository.getRecipeByIdServer(recipeId)
+        return remoteRecipe
+    }
+
+}
