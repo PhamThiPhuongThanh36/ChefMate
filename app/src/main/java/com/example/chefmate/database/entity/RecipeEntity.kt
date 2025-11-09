867089fc7d851c8a2d52638f06545ff8c717666f
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-11-09 15:00:26 +0700

    feat: add data class RecipeWithDetail and fun .toRecipeView

--- app/src/main/java/com/example/chefmate/database/entity/RecipeEntity.kt
+++ app/src/main/java/com/example/chefmate/database/entity/RecipeEntity.kt
@@ -1,8 +1,13 @@
 package com.example.chefmate.database.entity
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Relation
+import com.example.chefmate.model.CookingStepAddRecipeData
+import com.example.chefmate.model.IngredientInput
 import com.example.chefmate.model.Recipe
+import com.example.chefmate.model.RecipeView
 
 @Entity(tableName = "Recipes")
 data class RecipeEntity(
@@ -33,6 +38,57 @@ fun RecipeEntity.toRecipe(): Recipe {
         createdAt = this.createdAt,
         userName = "phuongthanh",
         userImage = "",
+        isLiked = true
+    )
+}
+
+data class RecipeWithDetails(
+    @Embedded val recipe: RecipeEntity,
+
+    @Relation(
+        parentColumn = "recipeId",
+        entityColumn = "recipeId"
+    )
+    val ingredients: List<IngredientEntity>,
+
+    @Relation(
+        parentColumn = "recipeId",
+        entityColumn = "recipeId"
+    )
+    val steps: List<StepEntity>,
+
+    @Relation(
+        parentColumn = "userId",
+        entityColumn = "userId"
+    )
+    val user: UserEntity?
+)
+
+fun RecipeWithDetails.toRecipeView(): RecipeView {
+    return RecipeView(
+        recipeId = recipe.recipeId,
+        recipeName = recipe.recipeName,
+        image = recipe.image,
+        cookingTime = recipe.cookingTime,
+        ration = recipe.ration,
+        ingredients = ingredients.map {
+            IngredientInput(
+                ingredientName = it.ingredientName,
+                weight = it.weight,
+                unit = it.unit
+            )
+        },
+        steps = steps.map {
+            CookingStepAddRecipeData(
+                indexStep = it.stepId ?: 0,
+                content = it.description
+            )
+        },
+        viewCount = recipe.viewCount,
+        likeQuantity = recipe.likeQuantity,
+        isPublic = recipe.isPublic,
+        createdAt = recipe.createdAt,
+        user = user!!.toUserView(),
         isLiked = true
     )
 }
